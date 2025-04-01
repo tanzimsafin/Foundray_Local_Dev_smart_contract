6bef493744f48045cf4e723cc2e640af25817546
tanzim_safin <tanzimhossainsafin756@gmail.com>
2025-04-01 13:37:39 +0600

    learning Foundry

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: Unlicense
+import  "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Contract is ERC20{ 
+    constructor() ERC20("MyToken", "MTK")  {
+       
+        
+    }
+    function mint(address owner,uint amount) public{
+        _mint(owner,amount);
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -9,15 +9,27 @@ contract TestContract is Test {
     Contract c;
 
     function setUp() public {
-        c = new Contract();
+        c = new Contract();//set up before running avobe functions
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testInc() public {
+        assertEq(uint256(1), uint256(1),"ok");
     }
-
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testMint() public{
+        c.mint(address(this),100);
+        assertEq(c.balanceOf(address(this)),100,"ok");
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), 0,"Ok");
+         c.mint(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,100);
+         assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4), 100,"Ok");
+    }
+     function testTransfer() public{
+        c.mint(address(this),100);
+        c.transfer(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,50);
+        assertEq(c.balanceOf(address(this)),50,"ok");
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),50,"ok");
+        vm.prank(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);//now after this next function  will call by the address given here
+        c.transfer(address(this),50);
+        assertEq(c.balanceOf(address(this)),100,"ok");
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),0,"ok");
     }
 }
