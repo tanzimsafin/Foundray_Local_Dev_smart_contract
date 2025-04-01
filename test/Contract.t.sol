df5e8269d647ab8a34f82e7ad208cb3db45b40d5
tanzim_safin <tanzimhossainsafin756@gmail.com>
2025-04-01 15:06:41 +0600

    add event test

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -7,7 +7,8 @@ import "src/Contract.sol";
 
 contract TestContract is Test {
     Contract c;
-
+    event Transfer(address indexed from , address indexed to,uint value);// decleare a event .. enevts helps to connect with observer like node JS to observe a transaction  **up to 3 fields indexed
+    event Approval(address indexed owner, address indexed spender, uint256 value);
     function setUp() public {
         c = new Contract();//set up before running avobe functions
     }
@@ -32,4 +33,38 @@ contract TestContract is Test {
         assertEq(c.balanceOf(address(this)),100,"ok");
         assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),0,"ok");
     }
+    function testApproval() public{
+        c.mint(address(this),100);
+        c.approve(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 25);
+        vm.prank(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        c.transferFrom(address(this),0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,20);
+        assertEq(c.balanceOf(address(this)),80,"ok");
+        assertEq(c.balanceOf(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),20,"ok");
+        assertEq(c.allowance(address(this),0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),5,"ok");
+
+    }
+    function test_RevertApproval() public{
+        c.mint(address(this),100);
+        c.approve(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,10);
+        vm.prank(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        c.transferFrom(address(this),0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 10);
+    }
+    function test_RevertTransfer()public{
+        c.mint(address(this),100);
+        c.transfer(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 100); 
+    }
+    function testEvents() public{
+        c.mint(address(this),100);
+        vm.expectEmit(true,true,false,true);//first declear that an emit will happend very soon//first two used indexed thats why true and data field
+        emit Transfer(address(this),0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,10);//then what i emited to be expected
+        c.transfer(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,10 );//then expected transfer event happend and compared with emit one
+    }
+    function test_ExpectEmitApprove() public {
+        c.mint(address(this),100);
+        vm.expectEmit(true, true, false, true);
+        emit Approval(address(this),
+        0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,10);
+        c.approve(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 10);
+    }
+    
 }
